0b39a796778e3ab51d9c55320b70cdda540af650
Cesar Eduardo Boza Gonzalez <cesarebg@bershka.com>
2023-09-28 16:55:30 +0200

    Add components and transtions

--- components/AboutBershka/Mobile/aboutBershka.component.js
+++ components/AboutBershka/Mobile/aboutBershka.component.js
@@ -1,5 +1,25 @@
 export default {
-    setup() {
+    props: {
+        index: {
+            type: Number,
+            required: true
+        },
+        info: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props) {
+        const isMouseEnter = ref(false);
 
+        function onShowMore () {
+            this.isMouseEnter = !this.isMouseEnter
+            this.$emit('sectionShow', this.isMouseEnter ? props.index : -1);
+        };
+
+        return {
+          isMouseEnter,
+          onShowMore
+        }
     }
 }
--- pages/ItxAboutBershka/index.vue
+++ pages/ItxAboutBershka/index.vue
@@ -1,11 +1,48 @@
-<template>
-    <div>
-        <topbar />
-        <about-bershka />
-    </div>
-</template>
 <script setup>
 import Topbar from '@/components/Topbar/index.vue'
 import AboutBershka from '@/components/AboutBershka/Mobile';
+import { ref } from 'vue'
+
+const sectionSelectedIdx = ref(-1);
+
+function onSectionSelected (idx) {
+    debugger;
+    const refKey = `about-bershka-${sectionSelectedIdx.value}`;
+    sectionSelectedIdx.value = idx;
+    if (sectionSelectedIdx.value === -1) {
+        const element = document.getElementById(refKey);
+        if (element) {
+            nextTick(() => {
+                element.scrollIntoView();
+            });
+        }
+    }
+
+}
 </script>
-<style></style>
+<template>
+    <div class="about-bsk-page">
+        <topbar />
+        <about-bershka
+            id="about-bershka-0"
+            :index="0"
+            :info="'LA MARCA LA MARCALA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA LA MARCA'"
+            v-if="sectionSelectedIdx === -1 || sectionSelectedIdx === 0"
+            @sectionShow="onSectionSelected"
+        />
+        <about-bershka
+            :index="1"
+            :info="'CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO CONCEPTO'"
+            id="about-bershka-1"
+            v-if="sectionSelectedIdx === -1 || sectionSelectedIdx === 1"
+            @sectionShow="onSectionSelected"
+        />
+        <about-bershka
+            :index="2"
+            :info="'TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET TARGET'"
+            id="about-bershka-2"
+            v-if="sectionSelectedIdx === -1 || sectionSelectedIdx === 2"
+            @sectionShow="onSectionSelected"
+        />
+    </div>
+</template>
